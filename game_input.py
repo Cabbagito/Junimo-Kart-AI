aab8c77177440192b41a388158d46e8d0d0e5927
Petar Kupusarević <petar.kupusarevic@gmail.com>
2022-10-19 21:56:26 +0200

    Getting Score from Screen and Digit Recognition Dataset

--- game_input.py
+++ game_input.py
@@ -16,3 +16,34 @@ def get_screen(size=(256, 256), region=(40, 72, 1235, 710)):
 
 def get_screen_image(size=(256, 256), region=(40, 72, 1235, 710)):
     return grab().convert("L").crop(region).resize(size)
+
+
+def get_screen_and_score(
+    size=(256, 256),
+    region=(40, 72, 1235, 710),
+    score_region=(100, 5, 200, 35),
+    num_digits=5,
+):
+    img = grab().convert("L").crop(region)
+    return img.resize(size), get_score(img.crop(score_region), num_digits=num_digits)
+
+
+def get_score(score_img, num_digits=5):
+    score = ""
+    for i in range(num_digits):
+        x = 4 + i * 15
+        digit_image = score_img.crop((x, 0, x + 15, 30))
+        digit = get_digit(digit_image)
+        if digit is not None:
+            score += digit
+        else:
+            break
+    return int(score)
+
+
+def get_digit(image):
+    from random import randint
+
+    return str(randint(0, 9))
+
+
